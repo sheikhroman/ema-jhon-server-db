f858682036d2cd27eb5ccb5da46758df158e282d
Roman <sheikhroman042@gmail.com>
2021-10-25 15:27:14 +0600

    :first

--- src/Firebase/firebase.config.js
+++ src/Firebase/firebase.config.js
@@ -1,5 +1,13 @@
 console.log(process.env);
 const firebaseConfig = {
+        // apiKey: "AIzaSyBPiI6fxaII67Qqupf_OO_fKnMm2WrLr4c",
+        // authDomain: "ema-jhon-2.firebaseapp.com",
+        // projectId: "ema-jhon-2",
+        // storageBucket: "ema-jhon-2.appspot.com",
+        // messagingSenderId: "659027862726",
+        // appId: "1:659027862726:web:267e91b5d1181bb01163c7"
+    
+    
     apiKey: process.env.REACT_APP_API_KEY,
     authDomain: process.env.REACT_APP_AUTH_DOMAIN,
     projectId: process.env.REACT_APP_PROJECT_ID,
